5ec2b66e97f3c8e15494f826e6dd9bb55e317cbc
mcjsjs <mcjsjs@naver.com>
2014-05-30 16:05:52 +0900

    board

--- homepagegit/src/homepage/board/BoardDto.java
+++ homepagegit/src/homepage/board/BoardDto.java
@@ -7,6 +7,7 @@ public class BoardDto {
 	private String day;
 	private int hit;
 	private int like;
+	private int test;
 	
 	
 	
---
+++ homepagegit/src/homepage/board/Gtitest.java
@@ -0,0 +1,5 @@
+package homepage.board;
+
+public class Gtitest {
+
+}
